e6d1aa95428fb7077a8e5a4e12d20dc2c9e8885c
rubini-sde <rubini.sde@gmail.com>
2024-11-29 23:26:34 -0800

    Update sortColors.js

--- Algorithms/Array/TwoPointers/sortColors.js
+++ Algorithms/Array/TwoPointers/sortColors.js
@@ -8,3 +8,25 @@ We will use the integers 0, 1, and 2 to represent the color red, white, and blue
 You must solve this problem without using the library's sort function.
  */
 
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let current = 0;
+
+    while(current <= right) {
+        if(nums[current] === 0) {
+            [nums[current], nums[left]] = [nums[left], nums[current]];
+            left++;
+            current++;
+        } else if(nums[current] === 1) {
+            current++;
+        } else if(nums[current] === 2) {
+            [nums[current], nums[right]] = [nums[right], nums[current]];
+            right--;
+        }
+    }
+};

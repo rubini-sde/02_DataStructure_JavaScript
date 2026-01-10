caf27b81a3a600a7a74de298b314c74e16441802
Rubini Dhanasekaran <rubini.sde@gmail.com>
2026-01-10 07:34:59 -0800

    Leetcode # 209

---
+++ Algorithms/Array/SlidingWindow/209_minimum_size_subarray_sum.ts
@@ -0,0 +1,40 @@
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+// Example 2:
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+// Example 3:
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+
+// Constraints:
+
+// 1 <= target <= 109
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+
+// Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+function minSubArrayLen(target: number, nums: number[]): number {
+  let left = 0;
+  let currentSum = 0;
+  let minLength = Infinity;
+
+  for (let right = 0; right < nums.length; right++) {
+    currentSum += nums[right];
+
+    while (currentSum >= target) {
+      minLength = Math.min(minLength, right - left + 1);
+      currentSum -= nums[left];
+      left++;
+    }
+  }
+  return minLength === Infinity ? 0 : minLength;
+}
